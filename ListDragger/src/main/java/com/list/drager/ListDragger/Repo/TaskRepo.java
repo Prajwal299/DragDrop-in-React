de5dd161031813aeef8c3a223635476a957b28bf
Prajwal Rawate <71627647+Prajwal299@users.noreply.github.com>
2024-01-10 23:11:20 +0530

    Delete ListDragger directory

--- ListDragger/src/main/java/com/list/drager/ListDragger/Repo/TaskRepo.java
+++
@@ -1,9 +0,0 @@
-package com.list.drager.ListDragger.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.list.drager.ListDragger.Entity.Task;
-
-public interface TaskRepo extends JpaRepository<Task, Long> {
-
-}
